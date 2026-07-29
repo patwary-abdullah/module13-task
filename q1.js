1d6b53d73149db37e2b140464a000e713c9364cf
Patwary-Abdullah <abdullah.acmeai@gmail.com>
2026-07-29 17:39:16 +0600

    Q1 is completed

---
+++ q1.js
@@ -0,0 +1,20 @@
+// Input: a number
+// Output: "Hot", "Cold", or "Normal"
+// Returns: a string
+ 
+function checkTemperature(temp) {
+    if(temp>=30){
+        return "Hot";
+    }
+    else if (temp<=15){
+        return "Cold";
+    }
+    else{
+        return "Normal";
+    }
+ 
+}
+ 
+console.log(checkTemperature(35)); // Expected: "Hot"
+console.log(checkTemperature(10)); // Expected: "Cold"
+console.log(checkTemperature(20)); // Expected: "Normal"
