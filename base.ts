1b518cc198549cb16543a7c28373d7ef69290fd9
Patwary-Abdullah <abdullah.acmeai@gmail.com>
2026-07-29 17:54:32 +0600

    Completed second question

--- base.ts
+++
@@ -1,4 +0,0 @@
-const message: string = "Hello, TypeScript!";
-console.log(message);
-
-export {};
---
+++ q2.js
@@ -0,0 +1,15 @@
+// Input: a number
+// Output: the number with digits reversed
+// Returns: a number
+ 
+function reverseNumber(num) {
+  let str = num.toString();
+  // TODO: build the reversed string, then convert back to a number
+  const revNum = str.split('').reverse().join('');
+//   console.log(typeof revNum);
+  return Number(revNum);
+ 
+}
+ 
+console.log(reverseNumber(1234)); // Expected: 4321
+console.log(reverseNumber(7));    // Expected: 7
